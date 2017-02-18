287d5808c6c819f28e49c7f3b280c4ba0a276d36
snowden <gagg1707@gmail.com>
2017-02-18 14:39:42 -0400

    modify database1

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
-//Modify reset.function(
+//Modify database1
 class ResetPasswordController extends Controller
 {
     /*
