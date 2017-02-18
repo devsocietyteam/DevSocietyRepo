87dabf7c7d83363f2af2d4258c64bd364542d28b
snowden <gagg1707@gmail.com>
2017-02-18 14:29:02 -0400

    first snowndede

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
-//SUPER COMENTARIO POR SNOWDEN
+//SUPER COMENTARIO POR SNOWDEN MODERFUCKER
 class ResetPasswordController extends Controller
 {
     /*
